fa9a8fcf2784182682f65ba6ca36d1a7d656553a
Jesus <jesus.cassano.2006@gmail.com>
2023-02-09 21:37:03 +0100

    tsx files add

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,11 @@
+import { Info } from "../../../features/info/info";
+import { Keyboard } from "../../../features/keyboard/keyboard";
+
+export default function App() {
+  return (
+    <>
+      <Info></Info>
+      <Keyboard></Keyboard>
+    </>
+  );
+}
